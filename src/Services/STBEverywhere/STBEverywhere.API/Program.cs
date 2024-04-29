50ad59290db934f2c95e3ae1cfc0fdb1920351f5
Oumaima Yahyaoui <oumaimayahyaoui53@gmail.com>
2024-04-29 19:42:22 +0100

    initial

--- src/Services/STBEverywhere/STBEverywhere.API/Program.cs
+++ src/Services/STBEverywhere/STBEverywhere.API/Program.cs
@@ -1,13 +1,16 @@
-using STBEverywhere.API;
 
+using STBEverywhere.API;
+using STBEverywhere.Application;
+using STBEverywhere.Infrastructure;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services
-    .AddApplicationServices(builder.Configuration)
+    .AddApplicationServices()
     .AddInfrastructureServices(builder.Configuration)
-    .AddApiServices(builder.Configuration);
+    .AddApiServices();
+    
 
 
 
