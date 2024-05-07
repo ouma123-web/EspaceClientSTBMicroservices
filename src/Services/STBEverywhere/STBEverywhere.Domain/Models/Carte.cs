8951b9556e4cf7fd05f1f9f22f3ae6dc2e771aed
Oumaima Yahyaoui <oumaimayahyaoui53@gmail.com>
2024-05-07 14:35:55 +0100

    initial

--- src/Services/STBEverywhere/STBEverywhere.Domain/Models/Carte.cs
+++ src/Services/STBEverywhere/STBEverywhere.Domain/Models/Carte.cs
@@ -8,24 +8,41 @@ namespace STBEverywhere.Domain.Models
     {
         public ClientId ClientId { get; set; }
         public string NumCarte { get; set; } = null!;
-
+        public decimal Solde { get; set; }
         public decimal CodeSecretCarte { get; set; }
 
         public DateOnly DateExpiration { get; set; }
 
-      /*  public static Carte Create(CarteId id, string numCarte, decimal codeSecretCarte, DateOnly dateExpiration)
-        {
-            var carte = new Carte
-            {
-                Id = id,
-                NumCarte = numCarte,
-                CodeSecretCarte = codeSecretCarte,
-                DateExpiration = dateExpiration
-            };
 
-            return carte;
+        public string ConsulterCarte()
+        {
+            return $"ClientId: {ClientId}, NumCarte: {NumCarte}, CodeSecretCarte: {CodeSecretCarte}, DateExpiration: {DateExpiration}, Solde: {Solde}";
         }
-      */
+
+
+
+
+
+
+
+
+
+
+
+
+        /*  public static Carte Create(CarteId id, string numCarte, decimal codeSecretCarte, DateOnly dateExpiration)
+          {
+              var carte = new Carte
+              {
+                  Id = id,
+                  NumCarte = numCarte,
+                  CodeSecretCarte = codeSecretCarte,
+                  DateExpiration = dateExpiration
+              };
+
+              return carte;
+          }
+        */
 
 
     }
