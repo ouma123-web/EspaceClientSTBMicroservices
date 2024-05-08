14409362ab7e95113aeab526b146dc20b220f947
Oumaima Yahyaoui <oumaimayahyaoui53@gmail.com>
2024-05-08 22:00:37 +0100

    initial

---
+++ src/Services/STBEverywhere/STBEverywhere.Domain/Events/TransactionType.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace STBEverywhere.Domain.Events
+{
+    public enum TransactionType
+    {
+        In,
+        Out
+    }
+}
--- src/Services/STBEverywhere/STBEverywhere.Domain/Models/Transaction.cs
+++ src/Services/STBEverywhere/STBEverywhere.Domain/Models/Transaction.cs
@@ -9,15 +9,20 @@ using System.Transactions;
 
 namespace STBEverywhere.Domain.Models
 {
-    public class Transaction : Aggregate<TransactionId>
+    public class Transaction : Entity<TransactionId>
     {
         private readonly List<Compte> _comptes = new();
         public IReadOnlyList<Compte> Comptes => _comptes.AsReadOnly();
 
         public string Visualisation { get; set; }
         public decimal Montant { get; set; }
+        public TransactionType Type { get; set; }
 
-       // public IList<CompteTransaction> CompteTransaction { get; set; }
-
+        public Transaction(TransactionType type, string visualisation, decimal montant)
+        {
+            Type = type;
+            Visualisation = visualisation;
+            Montant = montant;
+        }
     }
 }
