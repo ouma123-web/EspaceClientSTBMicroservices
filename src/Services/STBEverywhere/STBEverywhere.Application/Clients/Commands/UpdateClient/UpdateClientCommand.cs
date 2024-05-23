8f8bd15d6b21be0f9d3752d23eb744fa65385689
Oumaima Yahyaoui <oumaimayahyaoui53@gmail.com>
2024-05-23 10:00:42 +0100

    initial

---
+++ src/Services/STBEverywhere/STBEverywhere.Application/Clients/Commands/UpdateClient/UpdateClientCommand.cs
@@ -0,0 +1,17 @@
+﻿using BuildingBlocks.CQRS;
+using STBEverywhere.Application.Dtos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace STBEverywhere.Application.Clients.Commands.UpdateClient
+{
+    public record UpdateClientCommand(ClientDto Client)
+        : ICommand<UpdateClientResult>;
+
+    public record UpdateClientResult(bool Is)
+
+
+}
--- src/Services/STBEverywhere/STBEverywhere.Application/GlobalUsing.cs
+++ src/Services/STBEverywhere/STBEverywhere.Application/GlobalUsing.cs
@@ -1,12 +1,2 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace STBEverywhere.Application
-{
-    internal class GlobalUsing
-    {
-    }
-}
+﻿global using STBEverywhere.Domain.Models;
+global using STBEverywhere.Domain.ValueObjects;
